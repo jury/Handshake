97aba8e90a95ddc35da186abd26a78468d5a8330
Kyle <kodex@Mensa.local>
2008-10-08 00:12:03 -0700

    busy improvements

--- Classes/HSKMainViewController.m
+++ Classes/HSKMainViewController.m
@@ -128,6 +128,11 @@
     userBusy = NO;
 }
 
+- (void)viewWillDisappear:(BOOL)animated
+{
+	userBusy = YES;
+}
+
 - (void)popToSelf:(id)sender
 {
     [self.navigationController popToViewController:self animated:YES];
@@ -948,7 +953,6 @@
 
 - (void)checkQueueForMessages
 {
-//fucked until I can make it work without timer
 	if(!userBusy)
 	{		
 		//if we have a message in queue handle it
@@ -958,6 +962,7 @@
 			
 			//done with it so trash it
 			[self.messageArray removeObjectAtIndex: 0];
+//			[[NSUserDefaults standardUserDefaults] setObject: self.messageArray forKey:@"storedMessages"];
 		}	
 	} 
 }
@@ -1364,8 +1369,6 @@
 {
 	userBusy = NO;
 	[self.navigationController dismissModalViewControllerAnimated: NO];	
-	
-	
 }
 
 #pragma mark -
