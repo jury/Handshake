e034d8769f46443ab89294186f647be5cba945ff
Ian Baird <rtmfd@morbo.local>
2008-10-07 20:02:28 -0700

    Adding forgotten files.

---
+++ Classes/HSKNavigationController.h
@@ -0,0 +1,18 @@
+//
+//  HSKNavigationController.h
+//  Handshake
+//
+//  Created by Ian Baird on 10/7/08.
+//  Copyright 2008 Skorpiostech, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+extern NSString *HSKNavigationControllerDidDismissModal;
+
+@interface HSKNavigationController : UINavigationController 
+{
+
+}
+
+@end
---
+++ Classes/HSKNavigationController.m
@@ -0,0 +1,29 @@
+//
+//  HSKNavigationController.m
+//  Handshake
+//
+//  Created by Ian Baird on 10/7/08.
+//  Copyright 2008 Skorpiostech, Inc. All rights reserved.
+//
+
+#import "HSKNavigationController.h"
+#import "HSKMainViewController.h"
+
+NSString *HSKNavigationControllerDidDismissModal = @"HSKNavigationControllerDidDismissModal";
+
+@implementation HSKNavigationController
+
+- (void)dismissModalViewControllerAnimated:(BOOL)animated
+{
+    [super dismissModalViewControllerAnimated:animated];
+    
+    if ([self.parentViewController isKindOfClass:[UINavigationController class]])
+    {
+        NSLog(@"Notifying on modal dismiss");
+        
+        [[NSNotificationCenter defaultCenter] postNotificationName:HSKNavigationControllerDidDismissModal object:self userInfo:nil];
+    }
+}
+
+
+@end
